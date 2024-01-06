1cc0ac5ee1197bdfe7137f7bb43b5337ef91b902
MarieJosee13 <mariejosee1313@gmail.com>
2024-01-06 12:02:51 -0800

    Integration of axios and doing API call on SheCodes AI

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-function generateCookingRecipe(event) {
-  event.preventDefault();
-
-  new Typewriter("#recipe-header", {
-    strings: "Pouding chômeur traditionnel",
-    autoStart: true,
-    cursor: "",
-    delay: 1,
-  });
-
+function displayRecipe(response) {
   new Typewriter("#recipe-description", {
-    strings:
-      "150 g (1 tasse) de farine tout usage non blanchie <br> 7,5 ml (1 1/2 c. à thé) de poudre à pâte <br> 105 g (1/2 tasse) de sucre <br> 75 g (1/3 tasse) de beurre non salé, ramolli <br> 1 oeuf  <br> 60 ml (1/4 tasse) de lait",
+    strings: response.data.answer,
     autoStart: true,
     delay: 5,
   });
+}
+
+function generateCookingRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f6b43f078o010195155a4d3f220t84be";
+  let prompt = `Generate a delicious cooking recipe about ${instructionsInput.value}`;
+  let context =
+    "You are a simplistic chef that likes short and simple recipes in basic HTML and seperate each line with a <br/>. Please include the title in an <h3>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayRecipe);
 }
 let cookingFormElement = document.querySelector("#cooking-generator-form");
 cookingFormElement.addEventListener("submit", generateCookingRecipe);
