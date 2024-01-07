8bb42939236bb8aaf5e62d9b1f0d8d3110269ded
MarieJosee13 <mariejosee1313@gmail.com>
2024-01-06 20:55:14 -0800

    cleaned up my css in alphabetical order, made sure everything was formatted and added comments to my JS for clarity

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function displayRecipe(response) {
   });
 }
 
+//all actions in this function
 function generateCookingRecipe(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
@@ -15,11 +16,14 @@ function generateCookingRecipe(event) {
     "You are a chef that likes short and simple recipes in basic HTML and seperate each line always with <br/>. Please include the title in an <h3>";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  //removing container until API is called & adding a loading message with animation
   let recipeElement = document.querySelector("#recipe-container");
   recipeElement.classList.remove("hide-element");
   recipeDescription = document.querySelector("#recipe-description");
   recipeDescription.innerHTML = `<div class='blink'>Generating recipe for ${instructionsInput.value}, please wait...</div>`;
 
+  //calling API
   axios.get(apiUrl).then(displayRecipe);
 }
 let cookingFormElement = document.querySelector("#cooking-generator-form");
